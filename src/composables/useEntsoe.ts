49af8293def3ea9a896b86de3ae042b2cc1734e6
Halvor Nafstad <mr.nafstad@gmail.com>
2022-09-05 17:16:04 +0100

    adding dist

---
+++ src/composables/useEntsoe.ts
@@ -0,0 +1,5 @@
+/**
+ * api security token
+ * a8b4fbed-7c03-415a-b43d-0cbd7444e12a
+ */
+export {}
--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,8 @@ export default defineConfig({
   ],
   server: {
     port: 3000
-  }
+  },
+  base: process.env.NODE_ENV === 'production'
+  ? '/strom-smart/'
+  : '/'
 })
